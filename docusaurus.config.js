2eb3bf302e3cd01cd29edf5d0922bbe5155f6731
Michal Nowikowski <godfryd@gmail.com>
2021-05-28 06:59:22 +0200

    added blog art about 0.557 release

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,6 +121,21 @@ module.exports = {
       trackingID: 'UA-152860048-1',
       anonymizeIP: true,
     },
+    // algolia: {
+    //   apiKey: 'YOUR_API_KEY',
+    //   indexName: 'YOUR_INDEX_NAME',
+
+    //   // Optional: see doc section below
+    //   contextualSearch: true,
+
+    //   // Optional: see doc section below
+    //   appId: 'YOUR_APP_ID',
+
+    //   // Optional: Algolia search parameters
+    //   searchParameters: {},
+
+    //   //... other Algolia params
+    // },
   },
   plugins: [
     "@docusaurus/plugin-ideal-image",
